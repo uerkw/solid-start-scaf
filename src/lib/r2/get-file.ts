731cc39931da33281c8b2e757be509a914f14bcf
Zoey <38047633+zoephyr@users.noreply.github.com>
2024-02-21 01:24:34 -0500

    Add scaffold/harness for API functionalities

--- src/lib/r2/get-file.ts
+++ src/lib/r2/get-file.ts
@@ -1,3 +1,5 @@
+import { fakeR2Data } from "./fakeData"
+
 export interface GetFileR2Props{
     id: string
     fileName: string
@@ -5,23 +7,30 @@ export interface GetFileR2Props{
 
 export function getFileR2(params: GetFileR2Props){
     "use server"
-    console.log("Ran on server only!")
-    const R2_API_ROUTE = import.meta.env.R2_API_ROUTE
-    const R2_API_KEY = import.meta.env.R2_API_KEY
+    console.log("Server: Running Get R2 File | Function");
+    const R2_API_ROUTE = process.env.R2_API_ROUTE;
+    const R2_API_KEY = process.env.R2_API_KEY;
+    const returnObjArray  = fakeR2Data;
+    const numericalId = +params.id - 1; 
+    let returnObject = {};
+    if (numericalId in returnObjArray){
+        returnObject = returnObjArray[numericalId]
+    } else {
+        console.log("Error, object not found with id")
+    }
+    //const returnObj  = { id: params.id, fileName: params.fileName, R2_ROUTE:R2_API_ROUTE } 
+    //console.log(`R2 Route was ${R2_API_ROUTE}. R2 Key was: ${R2_API_KEY}`)
 
-    const returnObj  = { id: params.id, fileName: params.fileName, R2_ROUTE:R2_API_ROUTE } 
-    console.log(`R2 Route was ${R2_API_ROUTE}. R2 Key was: ${R2_API_KEY}`)
-
-    return(returnObj)
+    return(returnObject)
 }
 
 export function listFilesR2(){
-    "use server"
-    console.log("Ran on server!")
-    const R2_API_ROUTE = import.meta.env.R2_API_ROUTE
-    const R2_API_KEY = import.meta.env.R2_API_KEY
-    const returnObj  = [{first: "first", second: "second"}, {first: "first-2", second: "second-2"}] 
+    "use server";
+    console.log("Server: Running List R2 Files | Function");
+    const R2_API_ROUTE = process.env.R2_API_ROUTE;
+    const R2_API_KEY = process.env.R2_API_KEY;
+    const returnObj  = fakeR2Data;
 
-    console.log(`R2 Route was ${R2_API_ROUTE}. R2 Key was: ${R2_API_KEY}`)
+    //console.log(`R2 Route was ${R2_API_ROUTE}. R2 Key was: ${R2_API_KEY}`)
     return returnObj
 }
