97f3e5b660508042f23818ff7e764403b86695e2
Zoey <38047633+zoephyr@users.noreply.github.com>
2024-04-03 20:48:04 -0400

    Update dependencies

---
+++ app.config.ts
@@ -0,0 +1,19 @@
+// import { defineConfig } from "@solidjs/start/config";
+// import solid from "vite-plugin-solid";
+// import mdx from '@mdx-js/rollup';
+// import remarkGfm from 'remark-gfm';
+
+// export default defineConfig({
+//   plugins: [
+//     solid({ ssr: true }),
+//     mdx({
+//       remarkPlugins: [remarkGfm],
+//       jsxImportSource: "solid-jsx",
+//       providerImportSource: "solid-mdx",
+//     }),
+//   ],
+// });
+
+import { defineConfig } from "@solidjs/start/config";
+
+export default defineConfig({});
