7686c1e89751a785b20d9f9d6603346fd1ce701f
Zoey <38047633+zoephyr@users.noreply.github.com>
2024-04-04 06:44:04 -0400

    Fix uploading issue, add verbosity to logs

--- src/routes/api/minio/upload.ts
+++ src/routes/api/minio/upload.ts
@@ -1,6 +1,6 @@
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import type { APIEvent, APIHandler } from "@solidjs/start/server/types";
+import type { APIEvent } from "@solidjs/start/server";
 import chalk from "chalk";
 import { minio } from "~/lib/minio";
 import { json } from "@solidjs/router";
@@ -19,18 +19,20 @@ export async function POST({ params, request }: APIEvent) {
     // }
 
     const filename = `${uuidv4()}.png`;
-
+    console.log(chalk.blue(`Filename: ${filename}`))
     const command = new PutObjectCommand({
       Bucket: process.env.MINIO_BUCKET_NAME,
       Key: filename,
     });
-
+    console.log(chalk.blue(`Command: ${command}`))
     const signedUrl = await getSignedUrl(minio, command, { expiresIn: 300 });
     console.log(chalk.yellow(`Signed URL: ${signedUrl}`))
     console.log(chalk.green(`Success generating upload URL!`));
 
     return json({ url: signedUrl});
   } catch (err) {
-    console.log("error");
+    console.log("Error encountered");
+    console.log(err);
+    return json({error : "Encountered an error"})
   }
 }
